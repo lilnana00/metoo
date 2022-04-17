b9acc3f9093def44642d9fa98c4f34b9132e273c
lilnana00 <anastasianichirella@mail.ru>
2022-04-17 22:28:04 +1000

    Добавьте файлы проекта.

---
+++ metoo/Lk.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace metoo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Lk : ContentPage
+    {
+        public Lk()
+        {
+            InitializeComponent();
+        }
+    }
+}
