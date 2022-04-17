400aecfcb276d596099c4f951acf8575dd5b2db8
pearrycon <sonyabu2002@mail.ru>
2022-04-18 02:23:12 +1000

    переходы регистрации и входа
    я старалась честно

--- metoo/MainPage.xaml.cs
+++ metoo/MainPage.xaml.cs
@@ -14,11 +14,16 @@ namespace metoo
         {
             InitializeComponent();
             vhod_button.Clicked += Go_to_vhod;
+            reg_button.Clicked += Go_to_reg;
         }
 
         private async void Go_to_vhod(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new Vhod());
+        }
+        private async void Go_to_reg(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Reg());
         }
     }
 }
