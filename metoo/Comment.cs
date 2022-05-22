fefcffa96f1cd6e302886b368ae7d683c3fbe7f1
aannechkkaa <96425807+aannechkkaa@users.noreply.github.com>
2022-05-23 01:20:34 +1000

    ссори, вот бд для комментов

--- metoo/Comment.cs
+++ metoo/Comment.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using SQLite;
+
 namespace metoo
 {
+    [Table("Comments")]
     public class Comment
     {
-        public Comment()
-        {
-        }
+        [PrimaryKey, AutoIncrement, Column("_id")]
+        public int ID { get; set; }
+        public int CreatorID { get; set; }
+        public string EventID { get; set; }
+        public int datetime { get; set; }
+        public string Text { get; set; }
+
     }
 }
--- metoo/CommentReprository.cs
+++ metoo/CommentReprository.cs
@@ -1,10 +1,40 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using SQLite;
+
 namespace metoo
 {
     public class CommentReprository
     {
-        public CommentReprository()
+        SQLiteConnection database3;
+        public CommentReprository(string databasePath)
         {
+            database3 = new SQLiteConnection(databasePath);
+            database3.CreateTable<Comment>();
+        }
+        public int Count()
+        {
+            return database3.Table<Comment>().Count();
+        }
+        public IEnumerable<Comment> GetItems()
+        {
+            return database3.Table<Comment>().ToList();
+        }
+        public Comment GetItem(int id) => database3.Get<Comment>(id);
+        public int DeleteItem(int id)
+        {
+            return database3.Delete<Comment>(id);
+        }
+        public int SaveItem(Comment item)
+        {
+            if (item.ID != 0)
+            {
+                database3.Update(item);
+                return item.ID;
+            }
+            else
+            {
+                return database3.Insert(item);
+            }
         }
     }
 }
